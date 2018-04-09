2f9f0d20f74bda087204a1071795979538cc9b19
wenfeng_xi <wenfeng_xi@dnt.com.cn>
2018-04-09 17:51:00 +0800

    地址增删改查功能

---
+++ models/ids.js
@@ -0,0 +1,19 @@
+'use strict';
+
+import mongoose from 'mongoose';
+
+const idsSchema = new mongoose.Schema({
+    address_id: Number,
+});
+
+const Ids = mongoose.model('Ids', idsSchema);
+
+Ids.findOne((err,data) => {
+    if(!data){
+        const newIds = new Ids({
+            address_id: 0
+        })
+        newIds.save();
+    }
+})
+export default Ids
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 import user from './user';
+import address from './address';
 
 export default app => {
     app.use('/user', user);
+    app.use('/address', address);
 }

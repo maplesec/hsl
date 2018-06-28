ab5d70121618c419cf9aa1dd528f62f784b4cdc1
wenfeng_xi <wenfeng_xi@dnt.com.cn>
2018-06-28 15:57:40 +0800

    重构user-controller

--- controller/acl_user.js
+++ controller/acl_user.js
@@ -5,29 +5,47 @@ import UserModel from '../models/acl_user'
 import ResourceModel from '../models/acl_resource'
 import RoleModel from '../models/acl_role'
 import formidable from 'formidable'
+
 const cols = 'id name account'
+
+function allowedPermissions(user_id, resources){
+    return new Promise(function(resolve,reject){
+        global.acl.allowedPermissions(user_id, resources, function(err, obj){
+            resolve(obj);
+        })
+    })
+}
+
+function userRoles(user_id){
+    return new Promise(function(resolve, reject){
+        global.acl.userRoles(user_id, function(err, roles){
+            resolve(roles);
+        })
+    })
+}
+
+function addUserRoles(user_id, roles_to_add){
+    return new Promise(function(resolve,reject){
+        global.acl.addUserRoles(user_id, roles_to_add, function(err){
+            resolve(err);
+        })
+    })
+}
+
+function removeUserRoles (user_id, roles) {
+    return new Promise(function (resolve,reject) {
+        global.acl.removeUserRoles(user_id, roles, function (err) {
+            resolve(err);
+        })
+    })
+}
 
 class User extends BaseComponent{
     constructor(){
         super()
         this.addUser = this.addUser.bind(this);
     }
-
     async login(req, res, next) {
-        function _f1(user_id, resources){
-            return new Promise(function(resolve,reject){
-                global.acl.allowedPermissions(user_id, resources, function(err, obj){
-                    resolve(obj);
-                })
-            })
-        }
-        function _f2(user_id){
-            return new Promise(function(resolve, reject){
-                global.acl.userRoles(user_id, function(err, roles){
-                    resolve(roles);
-                })
-            })
-        }
         const form = new formidable.IncomingForm();
         form.parse(req, async (err, fields, files) => {
             const {account, password} = fields;
@@ -56,8 +74,8 @@ class User extends BaseComponent{
                     resourceList.forEach(function(item){
                         resources.push(item.id);
                     });
-                    const permissions = await _f1(id, resources);
-                    const roles = await _f2(id);
+                    const permissions = await allowedPermissions(id, resources);
+                    const roles = await userRoles(id);
                     const mix = { id, name, account, permissions, roles };
                     req.session.user_id = id;
                     res.send({
@@ -95,20 +113,6 @@ class User extends BaseComponent{
 
     async getProfile(req, res, next) {
         const user_id = req.session.user_id;
-        function _f1(user_id, resources){
-            return new Promise(function(resolve,reject){
-                global.acl.allowedPermissions(user_id, resources, function(err, obj){
-                    resolve(obj);
-                })
-            })
-        }
-        function _f2(user_id){
-            return new Promise(function(resolve, reject){
-                global.acl.userRoles(user_id, function(err, roles){
-                    resolve(roles);
-                })
-            })
-        }
         if(!user_id || !Number(user_id)){
             res.send({
                 status: 0,
@@ -123,8 +127,8 @@ class User extends BaseComponent{
             resourceList.forEach(function(item){
                 resources.push(item.id);
             });
-            const permissions = await _f1(user_id, resources);
-            const roles = await _f2(user_id);
+            const permissions = await allowedPermissions(user_id, resources);
+            const roles = await userRoles(user_id);
             const user_obj = await UserModel.findOne({id: user_id}, cols);
             const {id, name, account} = user_obj;
             const mix = { id, name, account, permissions, roles };
@@ -221,13 +225,6 @@ class User extends BaseComponent{
      */
     async addUser(req, res, next){
         const form = new formidable.IncomingForm();
-        function _f(user_id, roles_to_add){
-            return new Promise(function(resolve,reject){
-                global.acl.addUserRoles(user_id, roles_to_add, function(err){
-                    resolve(err);
-                })
-            })
-        }
         form.parse(req, async (err, fields, files) => {
             const {account, name, password, roles} = fields;
             try {
@@ -257,7 +254,7 @@ class User extends BaseComponent{
                 const user = await UserModel.create(newUser);
                 if (roles) {
                     // 指定用户角色
-                    await _f(user_id, roles);
+                    await addUserRoles(user_id, roles);
                 }
                 res.send({
                     status: 1,
@@ -283,20 +280,7 @@ class User extends BaseComponent{
      */
     async deleteUser (req, res, next) {
         const {user_id} = req.params;
-        function _f1 (user_id) {
-            return new Promise (function(resolve,reject) {
-                global.acl.userRoles(user_id, function (err, roles) {
-                    resolve(roles);
-                })
-            })
-        }
-        function _f2 (user_id, roles) {
-            return new Promise(function (resolve,reject) {
-                global.acl.removeUserRoles(user_id, roles, function (err) {
-                    resolve(err);
-                })
-            })
-        }
+
         if (!user_id || !Number(user_id)) {
             res.send({
                 status: 0,
@@ -307,8 +291,8 @@ class User extends BaseComponent{
         }
         try{
             await UserModel.findOneAndRemove({id: user_id});
-            const roles = await _f1(user_id);
-            await _f2(user_id, roles);
+            const roles = await userRoles(user_id);
+            await removeUserRoles(user_id, roles);
             res.send({
                 status: 1,
                 type: 'SUCCESS'
@@ -332,20 +316,6 @@ class User extends BaseComponent{
      */
     async getUserById(req, res, next){
         const user_id = req.params.user_id;
-        function _f1(user_id, resources){
-            return new Promise(function(resolve,reject){
-                global.acl.allowedPermissions(user_id, resources, function(err, obj){
-                    resolve(obj);
-                })
-            })
-        }
-        function _f2(user_id){
-            return new Promise(function(resolve, reject){
-                global.acl.userRoles(user_id, function(err, roles){
-                    resolve(roles);
-                })
-            })
-        }
         if(!user_id || !Number(user_id)){
             res.send({
                 status: 0,
@@ -360,8 +330,8 @@ class User extends BaseComponent{
             resourceList.forEach(function(item){
                 resources.push(item.id);
             });
-            const permissions = await _f1(user_id, resources);
-            const roles = await _f2(user_id);
+            const permissions = await allowedPermissions(user_id, resources);
+            const roles = await userRoles(user_id);
             const user_obj = await UserModel.findOne({id: user_id}, cols);
             const {id, name, account} = user_obj;
             const mix = { id, name, account, permissions, roles };
@@ -390,27 +360,6 @@ class User extends BaseComponent{
     async updateUser(req, res, next){
         const user_id = req.params.user_id;
         const form = new formidable.IncomingForm();
-        function _f1(user_id){
-            return new Promise(function(resolve,reject){
-                global.acl.userRoles(user_id, function(err, old_roles){
-                    resolve(old_roles);
-                })
-            })
-        }
-        function _f2(user_id, roles_to_remove){
-            return new Promise(function(resolve,reject){
-                global.acl.removeUserRoles(user_id, roles_to_remove, function(err){
-                    resolve(err);
-                })
-            })
-        }
-        function _f3(user_id, roles_to_add){
-            return new Promise(function(resolve,reject){
-                global.acl.addUserRoles(user_id, roles_to_add, function(err){
-                    resolve(err);
-                })
-            })
-        }
         form.parse(req, async(err, fields, files) => {
             const {name, password, roles} = fields;
             try{
@@ -434,9 +383,9 @@ class User extends BaseComponent{
                 }
                 await UserModel.update({id: user_id}, newUser)
                 if (roles) {
-                    const _now = await _f1(user_id);
-                    await _f2(user_id, _now);
-                    await _f3(user_id, roles);
+                    const _now = await userRoles(user_id);
+                    await removeUserRoles(user_id, _now);
+                    await addUserRoles(user_id, roles);
                 }
                 res.send({
                     status: 1,
